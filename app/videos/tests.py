a1d33cc60a4f3cfe763c5f08158d41a5d70e4a93
KIM SHINEDAY <kimshineday99@gmail.com>
2025-07-16 19:19:05 +0900

    🗃️ Models.py 작성 및 migrations, DRF setting

--- app/videos/tests.py
+++ app/videos/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework.test import APITestCase
+
+class VideoAPITestCase(APITestCase):
+    # 테스트 코드 케이스 실행 전 먼저 실행되는 함수 > 테스트 전 데이터 생성
+    # 1. 유저 생성 및 로그인
+    def setUp(self): pass
+
+    # api/v1/videos [GET]
+    def test_video_list_get(self): pass
+    def test_video_list_post(self): pass
+    # 특정 비디오 조회
+    def test_video_detail_get(self): pass
+    # 특정 비디오 업데이트(수정)
+    def test_video_detail_put(self): pass
+    # 특정 비디오 삭제
+    def test_video_detail_delete(self):pass
+    
--- app/videos/views.py
+++ app/videos/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+# Video와 관련된 REST API
+
+# 1. VideoList
+# api/v1/videos
+# [GET]: 전체 비디오 목록 조회
+# [POST]: 새로운 비디오 생성
+# [PUT], [DELETE] : X
+class VideoList(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+# 2. VideoDetail
+# api/v1/videos/{video_id}
+# [GET]: 특정 비디오 조회
+# [POST]: X
+# [PUT]: 특정 비디오 업데이트
+# [DELETE]: 특정비디오 삭제
+class VideoDetail(APIView):
+    def get(self, request):
+        pass
+
+    def put(self, request):
+        pass
